1e33f0637cd050f3b881d9974273a8e8f83e8972
MuspellGod <tanjaleef@gmail.com>
2026-08-19 23:33:19 +0530

    app js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const getUserInfoRoutes = require('./APIs/GetUserInfo/routes/getUserInfo.route');
 const updateUserInfoRoutes = require('./APIs/UpdateUserInfo/routes/updateUserInfo.route')
 const addAccountRequestRoutes = require('./APIs/AddAccountRequest/routes/addAccountRequest.route')
+const getAccountDetailRequest = require('./APIs/GetAccountDetailRequest/routes/getAccountDetailRequest.route')
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use('/', getUserInfoRoutes);
 app.use('/', updateUserInfoRoutes);
 app.use('/', addAccountRequestRoutes);
+app.use('/',getAccountDetailRequest);
 // Global error handler (must handle TMF error format for CTK)
 //app.use(require('./common/middleware/errorHandler'));
 
