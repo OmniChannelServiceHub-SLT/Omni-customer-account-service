9e2dc92b7cf648a3ac039237be0c3c1cdc057895
MuspellGod <tanjaleef@gmail.com>
2026-09-01 00:18:21 +0530

    files-running

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const VASgetProfile = require('./APIS/VAS_ListProfile/routes/route')
 const ISP_DIRECTgetProfile = require('./APIs/ISP_Direct_ListProfile/routes/route')
 const ISP_SOAgetProfile = require('./APIs/ISP_SOA_ListProfile/routes/route')
+const ISP_DIRECTpatchContact = require('./APIs/ISP_Direct_PatchContact/routes/route')
 
 const getUserInfoRoutes = require(
   './APIs/GetUserInfo/routes/getUserInfo.route'
@@ -41,6 +42,7 @@ app.get('/health', (req, res) => {
     port: Number(process.env.PORT || 3002),
   });
 });
+app.use('/',ISP_DIRECTpatchContact);
 app.use('/',ISP_SOAgetProfile);
 app.use('/',ISP_DIRECTgetProfile);
 app.use('/',VASgetProfile);
