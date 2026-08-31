192954c8375eda0870a004299fa71f3c4de2a56d
MuspellGod <tanjaleef@gmail.com>
2026-09-01 00:57:58 +0530

    files running-tested

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const ISP_DIRECTpatchContact = require('./APIs/ISP_Direct_PatchContact/routes/ro
 const ISP_SOApatchContact = require('./APIs/ISP_SOA_PatchContact/routes/route')
 const PEO_PEOProfile = require('./APIs/PEO_Get_PEOProfile/routes/route')
 const Dashboard_listProfileDistributionDashboard = require('./APIs/Dashboard_listProfileDistributionDashboard/routes/route')
+const AccountOmni_GetAccountDetailRequestChatBot = require('./APIs/AccountOmni_GetAccountDetailRequestChatBot/routes/route')
 
 const getUserInfoRoutes = require(
   './APIs/GetUserInfo/routes/getUserInfo.route'
@@ -45,6 +46,7 @@ app.get('/health', (req, res) => {
     port: Number(process.env.PORT || 3002),
   });
 });
+app.use('/',AccountOmni_GetAccountDetailRequestChatBot)
 app.use('/',Dashboard_listProfileDistributionDashboard);
 app.use('/',PEO_PEOProfile)
 app.use('/',ISP_SOApatchContact);
