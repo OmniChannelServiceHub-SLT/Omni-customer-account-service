0090b4a530bbd393816a2e66ef9410c51b0e8482
MuspellGod <tanjaleef@gmail.com>
2026-09-17 03:18:04 +0530

    files-server running

--- src/APIs/RemoveAccountRequest/services/service.js
+++ src/APIs/RemoveAccountRequest/services/service.js
@@ -5,5 +5,10 @@ exports.getAccountById = async (id) => {
 };
 
 exports.removeAccountById = async (id) => {
-  return await Account.findOneAndDelete({ id });
+ // return await Account.findOneAndDelete({ id });
+   return await Account.findOneAndUpdate(
+    { id, '@type': 'BillingAccount' },
+    { $set: { state: 'terminated' } },
+    { new: true }
+  );
 };
