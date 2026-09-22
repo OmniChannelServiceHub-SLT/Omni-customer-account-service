5e9906aa4bc2cae1756ee6dacdffcf47c1582907
MuspellGod <tanjaleef@gmail.com>
2026-09-22 14:59:15 +0530

    changed the route file to accept the middleware

--- src/APIs/ListExistCustomer/routes/route.js
+++ src/APIs/ListExistCustomer/routes/route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const controller = require('../controllers/controller');
-
+const resolveClientType = require('../../../common/middleware/resolveClientType');
 const router = express.Router();
 
 // TMF629-compliant GET customer by NIC
-router.get('/tmf-api/customerManagement/v4/customer', controller.checkExistCustomer);
+router.get('/tmf-api/customerManagement/v4/customer',resolveClientType, controller.checkExistCustomer);
 
 module.exports = router;
